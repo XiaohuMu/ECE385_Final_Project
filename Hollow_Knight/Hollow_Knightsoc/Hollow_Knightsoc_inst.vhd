0884ee8fa6bb081491bb2d48aec0fb1bbc8260f7
XiaohuMu <xiaohum2@illinois.edu>
2023-04-20 16:58:24 -0500

    update the qsf

---
+++ Hollow_Knight/Hollow_Knightsoc/Hollow_Knightsoc_inst.vhd
@@ -0,0 +1,13 @@
+	component Hollow_Knightsoc is
+		port (
+			clk_clk       : in std_logic := 'X'; -- clk
+			reset_reset_n : in std_logic := 'X'  -- reset_n
+		);
+	end component Hollow_Knightsoc;
+
+	u0 : component Hollow_Knightsoc
+		port map (
+			clk_clk       => CONNECTED_TO_clk_clk,       --   clk.clk
+			reset_reset_n => CONNECTED_TO_reset_reset_n  -- reset.reset_n
+		);
+
